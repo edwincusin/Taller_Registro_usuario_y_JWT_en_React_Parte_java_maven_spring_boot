831abeb95a3d1f7bcdb32f937f5ad36a37d829db
EDWINCSN <c.edwin32@hotmail.com>
2026-06-21 14:08:52 -0500

    feat(EGCA): (PeliculasController) => add enpoind PUT editar con imagen multipart/formdata

--- src/main/java/com/krakedev/taller_jwt/controller/PeliculaController.java
+++ src/main/java/com/krakedev/taller_jwt/controller/PeliculaController.java
@@ -2,11 +2,16 @@ package com.krakedev.taller_jwt.controller;
 
 import java.util.List;
 import java.util.Map;
+import java.util.Optional;
 
 import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Repository;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -56,4 +61,45 @@ public class PeliculaController {
 			return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(Map.of("Mensaje","Error interno del sistema al crear pelicula"));
 		}
 	}
+	
+	//METODO EDITAR PELICULA
+	@PutMapping("/{id}")
+	public ResponseEntity<?> editar(
+			@PathVariable("id") Integer id,
+			@RequestParam("titulo") String titulo,
+			@RequestParam("genero") String genero,
+			@RequestParam("sinopsis") String sinopsis,
+			@RequestParam(value="file", required=false) MultipartFile file
+			){
+		
+		try {
+			Optional<Pelicula> peliculaOptionalRecuperadaBDD=peliculaRepository.findById(id);
+			
+			if (peliculaOptionalRecuperadaBDD.isPresent()) {
+				Pelicula pelicula=peliculaOptionalRecuperadaBDD.get(); // aqui sacamos del envoltorio de optional el obejeto recuperado
+				//seteamos en el mismo objeto lo que nos viene del front
+				
+				pelicula.setTitulo(titulo);
+				pelicula.setGenero(genero);
+				pelicula.setSinopis(sinopsis);
+				
+				if(file!=null && !file.isEmpty()) {
+					pelicula.setFoto(file.getBytes());
+					pelicula.setMimeType(file.getContentType());
+				}
+				
+				peliculaRepository.save(pelicula);
+				return ResponseEntity.status(HttpStatus.OK).body(Map.of("Mensaje","Edision exitosa"));
+				
+			}else {
+				return ResponseEntity.status(HttpStatus.NOT_FOUND).body(Map.of("Mensjae","Pelicula no encontrada en la BDD"));
+			}
+			
+		} catch (Exception e) {
+			return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(Map.of("Mensaje","Error al realizar la edicion"));
+		}
+	}
+	
+	
+	
 }
